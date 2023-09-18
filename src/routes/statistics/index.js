26dffb1bf5a3dcdf85cd68e2a9fb48d02d785f2e
Leandro Andrade <leandro.andrade2588@gmail.com>
2023-09-18 09:31:13 -0300

    feat: add reference of statistics

--- src/routes/statistics/index.js
+++ src/routes/statistics/index.js
@@ -1,5 +1,6 @@
 module.exports = async (fastify, opts) => {
   fastify.get('/', async (req, reply) => {
+    // reference: https://node-oracledb.readthedocs.io/en/latest/user_guide/connection_handling.html#connpoolmonitor
     return fastify.oracle.pool.getStatistics();
   });
 };
